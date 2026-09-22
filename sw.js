bfdb1291665ba6745184a61e2dcf1e06758a4111
FernandoGalvez1959 <galvezcanalesfernando@gmail.com>
2026-09-22 07:12:21 +0200

    Add service worker for caching assets

---
+++ sw.js
@@ -0,0 +1,26 @@
+const CACHE = "agenda-semanal-v1";
+const ASSETS = ["./", "./index.html", "./manifest.json", "./icon-192.png", "./icon-512.png"];
+
+self.addEventListener("install", function(event){
+  event.waitUntil(
+    caches.open(CACHE).then(function(cache){ return cache.addAll(ASSETS); }).catch(function(){})
+  );
+  self.skipWaiting();
+});
+
+self.addEventListener("activate", function(event){
+  event.waitUntil(
+    caches.keys().then(function(keys){
+      return Promise.all(keys.filter(function(k){ return k!==CACHE; }).map(function(k){ return caches.delete(k); }));
+    })
+  );
+  self.clients.claim();
+});
+
+self.addEventListener("fetch", function(event){
+  event.respondWith(
+    caches.match(event.request).then(function(cached){
+      return cached || fetch(event.request).catch(function(){ return caches.match("./index.html"); });
+    })
+  );
+});
